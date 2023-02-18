3379fe6b5da6d776886f8c00317f959e633d1f3b
Ferdinand Cuisin <ferdinand.c@meta-api.io>
2023-02-18 09:34:27 +0100

    feat: retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  useEffect(() => {}, []);
+  const [quote, setQuote] = useState(null);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch("/time");
+        const data = await res.json();
+        setQuote(data.time);
+      } catch (error) {
+        return error;
+      }
+    })();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{quote}</p>
       </header>
     </div>
   );
